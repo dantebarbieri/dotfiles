75fb2285682c28c918957ffdf9fd9e09f397def4
pulchroxloom <dantevbarbieri@gmail.com>
2020-07-24 16:18:13 -0500

    Update configs dante-pc

--- fish/config.fish
+++ fish/config.fish
@@ -1,20 +1,25 @@
 alias la='ls -A --color=auto'
 alias ll='la -l --color=auto'
 alias c "clear"
-alias search "pacman -Ss"
+alias search "pacman -Ss --color always"
 alias search-aur "yay -Ss"
-alias pkgs "pacman -Qe"
+alias pkgs "pacman -Qe --color always"
 alias pkgs-aur "yay -Qe"
-alias install "sudo pacman -S"
+alias install "sudo pacman -S --color always"
 alias install-aur "yay -S"
-alias update "sudo pacman -Syu"
+alias update "sudo pacman -Syu --color always"
 alias update-aur "yay -Syu"
-alias rmv "sudo pacman -Rns"
+alias rmv "sudo pacman -Rns --color always"
 alias rmv-aur "yay -Rns"
 alias remove "rmv"
 alias remove-aur "rmv-aur"
-alias cache "sudo pacman -Sc"
+alias cache "sudo pacman -Sc --color always"
 alias cache "yay -Sc"
 alias .. "cd .."
 
+function fish_prompt
+    powerline-shell --shell bare $status
+end
+
 fish_ssh_agent
+neofetch
